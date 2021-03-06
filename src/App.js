942033a9800e1fbebd47a80110185ea7e19a90b6
Brent Yager <brentlyager@gmail.com>
2021-03-05 21:53:17 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          This is some sample text
+          This is some sample text again
         </p>
         <a
           className="App-link"
